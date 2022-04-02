d526b409d2a156d709c56595053693a1795eed47
澤野令 <sawanorei@sawanoreinoMacBook-Pro.local>
2022-04-02 22:25:34 +0900

    add:index.html app.py

--- iris/app/app.py
+++ iris/app/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session
 from wtforms import Form, FloatField, SubmitField, validators, ValidationError
 import numpy as np
 import joblib
@@ -7,6 +7,7 @@ import matplotlib.image as mpimg
 from PIL import Image
 import pyocr
 import sys
+from datetime import timedelta  # 時間情報を用いるため
 
 # 検索タブの方の実装は未着手
 
@@ -15,6 +16,10 @@ dic = {}
 count = 0
 
 app = Flask(__name__)
+
+app.secret_key = 'user'
+# app.permanent_session_lifetime = timedelta(
+#    minutes=5)  # -> 5分 #(days=5) -> 5日保存
 
 @app.route('/', methods=['GET', 'POST'])
 
@@ -26,7 +31,7 @@ def predicts():
     
     # 最初にURLを開く際には、画像ファイルが選択されてないので文字認識の動作を飛ばす
     if count != 0:
-        
+        print(request.form.get("img_file"))
         # まず入力された画像ファイルをカレントディレクトリに保存する
         # 保存時のファイル名は、入力されたファイルの名称が引き継がれるようにしたいが、やり方がわからない
         print(request.files['img_file'])
@@ -47,6 +52,8 @@ def predicts():
         # ターミナルに文字認識の結果を出力
         print(txt)
         # このあと、それぞれの行をdicに格納していくコードを書く
+        session.permanent = True
+        session['img_file'] = txt
 
     count += 1
     
@@ -57,6 +64,13 @@ def predicts():
  
     # else:
     #     return render_template('index.html')
+
+
+@app.route('/test', methods=['GET', 'POST'])
+def test():
+    print(session['img_file'])
+    return render_template('index.html')
+    
 
 # main関数
 if __name__ == "__main__":
